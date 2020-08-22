25be6e10ea88343c1e482ce271dd71ab3be784a2
vitoriafrds <vit.ferreiradossantos@gmail.com>
2020-08-22 20:02:26 -0300

    termino da estilizacao slide e botoes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 
-$('.slider-principal').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true
+});
